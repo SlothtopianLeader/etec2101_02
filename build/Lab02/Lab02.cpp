9f6358463869da2bfdb602721f738cc6022df6ad
SlothtopianLeader <salyers.audrianna@gmail.com>
2025-02-06 14:02:43 -0500

    Update 1 (2/6/2025)

--- build/Lab02/Lab02.cpp
+++ build/Lab02/Lab02.cpp
@@ -2,19 +2,16 @@
 //
 
 #include <iostream>
+#include <string>
+#include "array_list.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    ssuds::ArrayList<int> ilist;
+    ilist.append(15);
+    ilist.append(16);
+    ilist.prepend(14);
+    ilist.prepend(13);
+
+    ssuds::ArrayList<std::string> slist;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- build/Lab02/array_list.h
+++ build/Lab02/array_list.h
@@ -10,6 +10,8 @@
 
 namespace ssuds
 {
+	template <class T>
+
 	/// <summary>
 	/// This ArrayList class is an example of OOP techniques.
 	/// </summary>
@@ -24,7 +26,11 @@ namespace ssuds
 		/// <summary>
 		/// This is the (unique) ID# of this Person
 		/// </summary>
-		int index;
+		T* mArray;
+		unsigned int mArray_capacity;
+		unsigned int mArray_size;
+
+		unsigned int mDouble_array_capacity[mArray_size * 2]
 
 	public:
 		// This is the CONSTRUCTOR -- a method that has no return type and MUST
@@ -45,7 +51,12 @@ namespace ssuds
 		/// <param name="start_id">This is the id# for the new Person</param>
 		/// <param name="start_fname">This is the first name for the new Person</param>
 		/// <param name="start_lname">This is the last name for the new Person</param>
-		ArrayList(int start_id, std::string start_fname, std::string start_lname, float start_rate, unsigned int start_hours);
+		ArrayList()
+		{
+			mArray_capacity = 5;
+			mArray = new T[mArray_capacity];
+			mArray_size = 0;
+		}
 
 		// This is the DESTRUCTOR. This is called when an instance is about to go "away",
 		// This method gets called. It is unusual to call this manually. More often, it's
@@ -56,7 +67,46 @@ namespace ssuds
 
 		// This if a METHOD -- a function that all instances of the class can
 		// use. This is a DECLARATION.
-		const int append();
+
+		int reserve(ArrayList new_items)
+		{
+
+		}
+
+		const int append(ArrayList new_array_list)
+		{
+			for (unsigned int i = 0; i < mArray_size; i++)
+			{
+				if (mArray_size == mArray_capacity)
+				{
+
+				}
+				ArrayList* new_array = new ArrayList[mArray_size + 1];
+
+				for (unsigned int i = 0; i < mArray_size; i++)
+					// Add some content here.
+			}
+		}
+		/*
+		void PersonDatabase::add_person(Person new_person)
+		{
+			for (unsigned int i = 0; i < person_array_size; i++)
+			{
+				if (person_array[i].get_id() == new_person.get_id())
+					throw std::runtime_error("Error: Duplicate ID. Please enter a valid ID.");
+			}
+			Person* new_array = new Person[person_array_size + 1];
+
+			for (unsigned int i = 0; i < person_array_size; i++)
+				new_array[i] = person_array[i];
+
+			new_array[person_array_size] = new_person;
+
+			delete[] person_array;
+			person_array = new_array;
+			person_array_size++;
+		}
+		*/
 		const int prepend();
 		const int insert();
 		int at();
@@ -66,8 +116,18 @@ namespace ssuds
 		const bool find(unsigned int index);
 
 		// These are examples of GETTERS
-		unsigned int size;
-		unsigned int capacity;
+		int ssuds::Person::get_item()
+		{
+			return item;
+		}
+		unsigned int size()
+		{
+			return mArray_size;
+		}
+		unsigned int capacity()
+		{
+			return mArray_capacity;
+		}
 	};
 }
 #endif
